1259411f03c8301383d7c3ecd72eae155c945409
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-02-23 13:20:19 +0530

    Setup proper Maven structure for UC2

---
+++ src/test/java/com/apps/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -0,0 +1,50 @@
+package com.apps.QuantityMeasurementApp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.apps.QuantityMeasurementApp.QuantityMeasurementApp.Feet;
+import com.apps.QuantityMeasurementApp.QuantityMeasurementApp.Inches;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Feet f1 = new Feet(1.0);
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+        Feet f1 = new Feet(1.0);
+        String other = "NotFeet";
+        assertFalse(f1.equals(other));
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+        Feet f1 = new Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+        assertTrue(i1.equals(i2));
+    }
+}
